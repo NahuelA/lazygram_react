c2d5a22cf09343adbd858ff5ca9a23ef05d3d588
NahuelA <nahuelarrascaeta22@gmail.com>
2022-11-18 16:08:49 -0300

    Added function to put cache

--- src/utils/users/cache.js
+++ src/utils/users/cache.js
@@ -1,14 +1,12 @@
 // Function to add our give data into cache
-const addCache = (name, url, value) => {
+const addCache = async (name, url, value) => {
   // Converting our response into Actual Response form
   const data = new Response(JSON.stringify(value));
 
   if ("caches" in window) {
     // Opening given cache and putting our data into it
-    caches.open(name).then((cache) => {
-      console.log(cache);
-      cache.put(url, data);
-      alert("Data Added into cache!");
+    await caches.open(name).then(async (cache) => {
+      await cache.put(url, data);
     });
   }
 };
