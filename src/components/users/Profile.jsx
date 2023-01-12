bdcc4432a7c5846ab3f3a64b4cf7f83b71782702
NahuelA <nahuelarrascaeta22@gmail.com>
2023-01-12 01:47:03 -0300

    Fix bug | follow state

--- src/components/users/Profile.jsx
+++ src/components/users/Profile.jsx
@@ -2,17 +2,17 @@
 
 import { lgApi } from "../../__modules__";
 import "../../css/users/Profile.css";
+import { BsFillBookmarkFill, BsFillGrid3X3GapFill } from "react-icons/bs";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import "../../css/Margin.css";
 
 const Profile = () => {
   /* STATES */
-  // For cancel load request
-  const cancelToken = axios.CancelToken.source();
   // Profile
   const [profile, setProfile] = useState([]);
   // Posts from current profile
@@ -22,48 +22,59 @@ const Profile = () => {
   const [followings, setFollowings] = useState([]);
 
   // Edit or follow btn
-  const [editOrFollowBtn, setEditOrFollowBtn] = useState("Edit profile");
-  // Edit or follow htmlClass
-  const [editOrFollowClass, setEditOrFollowClass] = useState(
-    "ml-5 btn btn-sm btn-outline-info"
-  );
+  const [editOrFollowBtn, setEditOrFollowBtn] = useState("Follow");
+  const authProfile = window.localStorage.getItem("profile_auth");
 
-  // Follow or Unfollow btn
-  const [followOrUnfollow, setFollowOrUnfollow] = useState("Follow");
-  // Follow or Unfollow htmlClass
-  const [followClass, setFollowClass] = useState("ml-5 btn btn-primary");
+  // Edit or follow className
+  const [editOrFollowClass, setEditOrFollowClass] = useState(
+    "ml-5 btn btn-sm btn-primary"
+  );
 
   // Params from url
   const kwargUsername = useParams();
-  const profile_auth = window.localStorage.getItem("profile_auth");
 
   // Context
   const { accessToken } = useContext(AuthContext);
 
   // If picture is null, show default profile picture
   const picture =
-    profile[0]?.picture !== null
-      ? profile[0]?.picture
+    profile?.picture !== null
+      ? profile?.picture
       : "http://localhost:8000/media/user_circle.svg";
+
+  /**
+   * Fetch to profile
+   */
+  const getProfile = async () => {
+    // Get own profile
+    await lgApi(`accounts/profiles/${kwargUsername.username}`, {
+      headers: {
+        Authorization: "Bearer " + String(accessToken),
+      },
+    }).then((res) => {
+      setProfile(res.data);
+    });
+  };
 
   /**
    * If a profile is followed, change followOrUnfollow state to "Follow" or "Unfollow"
    */
   const isFollowed = async () => {
-    await lgApi(`accounts/isfollowed/${profile_auth}`, {
+    await lgApi(`accounts/isfollowed/${authProfile}`, {
       method: "get",
       headers: {
         Authorization: "Bearer " + String(accessToken),
       },
       params: {
-        profile_followed: kwargUsername.username,
+        myprofile: kwargUsername.username,
       },
     })
       .then((res) => {
         if (res.data) {
-          setFollowOrUnfollow("Unfollow");
+          setEditOrFollowBtn("Unfollow");
+          setEditOrFollowClass("ml-5 btn btn-sm btn-info");
         } else {
-          setFollowOrUnfollow("Follow");
+          setEditOrFollowBtn("Follow");
         }
       })
       .catch((err) => {
@@ -73,68 +84,56 @@ const Profile = () => {
 
   /**
    * If is your account profile, edit. Else follow.
-   * @param {event} e
    */
-  const EditOrFollow = async (e) => {
-    if (kwargUsername.username !== profile_auth) {
-      isFollowed();
-
-      // Follow
-      await lgApi(`accounts/followers/${profile_auth}/`, {
-        method: "put",
-        headers: {
-          Authorization: "Bearer " + String(accessToken),
-        },
-        data: {
-          profile: kwargUsername.username, // Profile that will be followed
-          followers: profile_auth, // Authenticated profile will be follow the profile
-        },
-      })
-        .then((res) => {
-          setFollowers([res.data]);
-        })
-        .catch((err) => {
-          console.error(err.response);
-        });
-
-      // Following
-      lgApi(`accounts/followings/${profile_auth}/`, {
-        method: "put",
-        headers: {
-          Authorization: "Bearer " + String(accessToken),
-        },
-        data: {
-          profile: profile_auth,
-          following: kwargUsername.username,
-        },
-      })
-        .then((res) => {
-          setFollowings([res.data]);
-        })
-        .catch((err) => {
-          console.error(err.response);
-        });
-    }
-  };
-
-  useEffect(() => {
-    // For cancel load request
-    const cancelToken = axios.CancelToken.source();
-
-    // Get own profile
-    lgApi(`accounts/profiles/${kwargUsername.username}`, {
+  const EditOrFollow = async () => {
+    // Follow
+    await lgApi(`accounts/followers/${authProfile}/`, {
+      method: "put",
       headers: {
         Authorization: "Bearer " + String(accessToken),
       },
-    }).then((res) => {
-      setProfile([res.data.results[0]]);
-    });
+      data: {
+        profile: kwargUsername.username, // Profile that will be followed
+        followers: authProfile, // Authenticated profile will be follow the profile
+      },
+    })
+      .then((res) => {
+        setFollowers([res.data]);
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
 
+    // Following
+    await lgApi(`accounts/followings/${authProfile}/`, {
+      method: "put",
+      headers: {
+        Authorization: "Bearer " + String(accessToken),
+      },
+      data: {
+        profile: authProfile,
+        following: kwargUsername.username,
+      },
+    })
+      .then((res) => {
+        setFollowings([res.data]);
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
+  };
+
+  // Fetch profile
+  useEffect(() => {
+    // For cancel load request
+    const cancelToken = axios.CancelToken.source();
+    getProfile();
     return () => {
       cancelToken.cancel();
     };
   }, [accessToken, kwargUsername.username]);
 
+  //Fetch followers and followings
   useEffect(() => {
     // For cancel load request
     const cancelToken = axios.CancelToken.source();
@@ -154,7 +153,7 @@ const Profile = () => {
         Authorization: "Bearer " + String(accessToken),
       },
     }).then((res) => {
-      setFollowings([res.data]);
+      setFollowings(res.data);
     });
 
     return () => {
@@ -162,6 +161,7 @@ const Profile = () => {
     };
   }, [accessToken, kwargUsername.username]);
 
+  // Fetch profile posts
   useEffect(() => {
     // For cancel load request
     const cancelToken = axios.CancelToken.source();
@@ -172,7 +172,7 @@ const Profile = () => {
       },
     })
       .then((res) => {
-        setPosts(res.data)
+        setPosts(res.data);
       })
       .catch((err) => {
         console.error(err);
@@ -183,14 +183,30 @@ const Profile = () => {
     };
   }, [accessToken, kwargUsername.username]);
 
+  // Set follow or edit profile
+  useEffect(() => {
+    let cancelToken = axios.CancelToken.source();
+
+    if (profile?.user?.username === authProfile) {
+      setEditOrFollowBtn("Edit profile");
+      setEditOrFollowClass("ml-5 btn btn-sm btn-outline-info");
+    } else {
+      isFollowed();
+    }
+
+    return () => {
+      cancelToken.cancel();
+    };
+  }, [profile, authProfile, accessToken, kwargUsername.username, isFollowed]);
+
   return (
     <main>
-      <div className="container mb-3">
+      <div className="container mb-3 mt-3">
         <div className="row">
           <div className="col-sm-4 d-flex justify-content-center">
             <img
               src={picture}
-              alt={profile[0]?.user?.username}
+              alt={profile?.user?.username}
               className="rounded-circle picture-profile"
               width="170px"
               height="170px"
@@ -199,38 +215,86 @@ const Profile = () => {
 
           <div className="col-sm-8">
             <h2 className="username-h2">
-              {profile[0]?.user?.username}
-              <span
-                className={editOrFollowClass}
-                onClick={() => EditOrFollow()}
-              >
-                {editOrFollowBtn}
-              </span>
+              {profile?.user?.username}
+
+              {/* Edit or follow */}
+              {profile?.user?.username === authProfile ? (
+                <Link to={`edit`} relative="path">
+                  <span className={editOrFollowClass}>{editOrFollowBtn}</span>
+                </Link>
+              ) : (
+                <span
+                  className={editOrFollowClass}
+                  onClick={() => {
+                    EditOrFollow();
+                  }}
+                >
+                  {editOrFollowBtn}
+                </span>
+              )}
             </h2>
 
             <div className="row mt-2 info-profile">
               <div className="col-sm-4">
-                <b>{profile[0]?.posts_count}</b> posts
+                <b>{profile?.posts_count}</b> posts
               </div>
               <div className="col-sm-4">
-                <b>{followers[0]?.followers_length}</b> followers
+                <b>{followers?.followers_length}</b> followers
               </div>
               <div className="col-sm-4">
-                <b>{followings[0]?.following_length}</b> following
+                <b>{followings?.following_length}</b> following
               </div>
             </div>
             <div className="row mt-4">
               <div className="col-sm-12">
-                <p>{profile[0]?.biography}</p>
+                <p>{profile?.biography}</p>
               </div>
               <div className="col-sm-12">
-                <p>Website: {profile[0]?.website}</p>
+                <p>Website: {profile?.website}</p>
               </div>
             </div>
           </div>
         </div>
       </div>
       <hr />
+      <div className="show-posts">
+        <span>
+          <BsFillGrid3X3GapFill
+            className="icons-posts"
+            onClick={(e) => {
+              lgApi(`profile-posts/${kwargUsername.username}`, {
+                headers: {
+                  Authorization: "Bearer " + String(accessToken),
+                },
+              })
+                .then((res) => {
+                  setPosts(res.data);
+                })
+                .catch((err) => {
+                  console.error(err);
+                });
+            }}
+          />
+        </span>
+        <span>
+          <BsFillBookmarkFill
+            className="icons-posts"
+            onClick={async (e) => {
+              await lgApi(`saved_posts/${authProfile}`, {
+                headers: {
+                  Authorization: "Bearer " + String(accessToken),
+                },
+              })
+                .then((res) => {
+                  setPosts(res.data.saved_post);
+                })
+                .catch((err) => {
+                  console.error(err);
+                });
+            }}
+          />
+        </span>
+      </div>
       <div className="own-posts">
         {posts?.map((value) => {
           return (
